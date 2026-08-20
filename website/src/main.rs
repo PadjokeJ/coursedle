63901c4494ac1a08ebf2f773ba174d8c8ade86d7
PadjokeJ <jonatan@padjokej.dev>
2026-08-20 16:00:32 +0200

    feat: coursedle start website

--- website/src/main.rs
+++ website/src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::fs::FileServer;
+use rocket_dyn_templates::{Template, context};
+
+#[macro_use] extern crate rocket;
+
+#[get("/")]
+fn index() -> Template {
+    Template::render("index", context! {})
+}
+/* 
+#[get("/id/<id>")]
+fn api_course(id: String) -> &'static str {
+  "hello"
+}*/
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+    .mount("/", routes![index])
+    .mount("/static", FileServer::from("/static"))
 }
---
+++ website/static/coursedle.js
@@ -0,0 +1,16 @@
+/*
+<tr class="guess">
+    <td>ID</td>
+    <td>Name</td>
+    <td>Language</td>
+    <td>Credits</td>
+    <td>Semester</td>
+    <td>Section</td>
+    <td>Exam session</td>
+</tr>
+*/
+
+function createRow(course) {
+    tbody = document.getElementById("tbody");
+    
+}
